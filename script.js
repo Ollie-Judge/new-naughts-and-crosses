18fe36966e9365557574e916054c85dd23d21e46
Ollie-Judge <ollie.judge@hotmail.com>
2023-12-04 17:00:47 +0000

    defined a class of player

---
+++ script.js
@@ -0,0 +1,34 @@
+const gameBoardObject = {
+  gameBoardArray: [
+    "square1",
+    "square2",
+    "square3",
+    "square4",
+    "square5",
+    "square6",
+    "square7",
+    "square8",
+    "square9",
+  ],
+};
+
+function Player(userType, counterType, score, read) {
+  this.userType = userType;
+  this.counterType = counterType;
+  this.playerChoices = [];
+  this.score = score;
+}
+
+const player1 = {
+  userType: "",
+  counterType,
+  playerChoices,
+  score,
+};
+
+const player2 = {
+  userType: "",
+  counterType,
+  playerChoices,
+  score,
+};
